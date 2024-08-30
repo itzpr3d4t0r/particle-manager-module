d87d7024c605129849199fd8a145b292d9d5dd8d
itzpr3d4t0r <103119829+itzpr3d4t0r@users.noreply.github.com>
2024-08-30 18:53:59 +0200

    Add float conversion functions

--- src/include/base.h
+++ src/include/base.h
@@ -241,6 +241,113 @@ TwoDoublesFromFastcallArgs(PyObject *const *args, Py_ssize_t nargs, double *val1
     return 0;
 }
 
+static FORCEINLINE int
+FloatFromObj(PyObject *obj, float *val)
+{
+    if (PyFloat_Check(obj)) {
+        *val = (float)PyFloat_AS_DOUBLE(obj);
+        return 1;
+    }
+
+    *val = (float)PyLong_AsLong(obj);
+    if (PyErr_Occurred()) {
+        PyErr_Clear();
+        return 0;
+    }
+
+    return 1;
+}
+
+/*Assumes obj is a Sequence, internal or conscious use only*/
+static FORCEINLINE int
+_FloatFromObjIndex(PyObject *obj, int index, float *val)
+{
+    int result = 0;
+
+    PyObject *item = PySequence_ITEM(obj, index);
+    if (!item) {
+        PyErr_Clear();
+        return 0;
+    }
+    result = FloatFromObj(item, val);
+    Py_DECREF(item);
+
+    return result;
+}
+
+static FORCEINLINE int
+FloatFromObjIndex(PyObject *obj, int index, float *val)
+{
+    int result = 0;
+
+    if ((PyTuple_Check(obj) || PyList_Check(obj)) &&
+        index < PySequence_Fast_GET_SIZE(obj)) {
+        result = FloatFromObj(PySequence_Fast_GET_ITEM(obj, index), val);
+    }
+    else {
+        PyObject *item = PySequence_GetItem(obj, index);
+
+        if (!item) {
+            PyErr_Clear();
+            return 0;
+        }
+        result = FloatFromObj(item, val);
+        Py_DECREF(item);
+    }
+
+    return result;
+}
+
+static FORCEINLINE int
+TwoFloatsFromObj(PyObject *obj, float *val1, float *val2)
+{
+    Py_ssize_t length;
+    /*Faster path for tuples and lists*/
+    if (PyTuple_Check(obj) || PyList_Check(obj)) {
+        length = PySequence_Fast_GET_SIZE(obj);
+        PyObject **f_arr = PySequence_Fast_ITEMS(obj);
+        if (length == 2) {
+            if (!FloatFromObj(f_arr[0], val1) || !FloatFromObj(f_arr[1], val2)) {
+                return 0;
+            }
+        }
+        else if (length == 1) {
+            /* Handle case of ((x, y), ) 'nested sequence' */
+            return TwoFloatsFromObj(f_arr[0], val1, val2);
+        }
+        else {
+            return 0;
+        }
+    }
+    else if (PySequence_Check(obj)) {
+        length = PySequence_Length(obj);
+        if (length == 2) {
+            if (!_FloatFromObjIndex(obj, 0, val1)) {
+                return 0;
+            }
+            if (!_FloatFromObjIndex(obj, 1, val2)) {
+                return 0;
+            }
+        }
+        else if (length == 1 && !PyUnicode_Check(obj)) {
+            /* Handle case of ((x, y), ) 'nested sequence' */
+            PyObject *tmp = PySequence_ITEM(obj, 0);
+            int ret = TwoFloatsFromObj(tmp, val1, val2);
+            Py_DECREF(tmp);
+            return ret;
+        }
+        else {
+            PyErr_Clear();
+            return 0;
+        }
+    }
+    else {
+        return 0;
+    }
+
+    return 1;
+}
+
 // these return PyObject * on success and NULL on failure.
 
 static FORCEINLINE PyObject *
