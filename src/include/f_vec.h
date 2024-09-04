e577bf6d8f1397a16b3223c1be82dcd56ff7fdec
itzpr3d4t0r <103119829+itzpr3d4t0r@users.noreply.github.com>
2024-09-04 16:04:12 +0200

    test change

--- src/include/f_vec.h
+++ src/include/f_vec.h
@@ -7,22 +7,21 @@
         Py_ssize_t capacity;       \
     } name##_arr;
 
-#define DEFINE_ARR_FUNCTIONS(T, name)                       \
-    int name##arr_init(name##_arr *v, Py_ssize_t capacity)  \
-    {                                                       \
-        v->capacity = capacity;                             \
-        v->data = PyMem_New(T, capacity);                   \
-        if (!v->data)                                       \
-            return 0;                                       \
-        return 1;                                           \
-    }                                                       \
-                                                            \
-    void name##arr_free(name##_arr *v)                      \
-    {                                                       \
-        PyMem_Free(v->data);                                \
-        v->capacity = 0;                                    \
+#define DEFINE_ARR_FUNCTIONS(T, name)                      \
+    int name##arr_init(name##_arr *v, Py_ssize_t capacity) \
+    {                                                      \
+        v->capacity = capacity;                            \
+        v->data = PyMem_New(T, capacity);                  \
+        if (!v->data)                                      \
+            return 0;                                      \
+        return 1;                                          \
+    }                                                      \
+                                                           \
+    void name##arr_free(name##_arr *v)                     \
+    {                                                      \
+        PyMem_Free(v->data);                               \
+        v->capacity = 0;                                   \
     }
-
 
 DEFINE_ARR_STRUCT(float, f)
 DEFINE_ARR_FUNCTIONS(float, f)
